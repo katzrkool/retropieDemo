c63393260aa44d7a5f6e82bceb60921db598169e
Lucas Kellar <katzrkool@protonmail.com>
2018-04-25 13:57:26 -0500

    uh oh

--- demo.py
+++ demo.py
@@ -21,7 +21,7 @@ def playGame(rom):
     p = subprocess.Popen(["{}/run.sh".format(currentDir), core, rom])
     try:
         p.wait(60)
-        if p.returncode != 139:
+        if p.returncode == 139:
             import sys
             sys.exit()
     except subprocess.TimeoutExpired:
